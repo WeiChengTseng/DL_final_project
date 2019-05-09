30c9cb5e8a32b8fe0bb71dab1825587e8c3b752c
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-09 23:31:13 +0800

    update unity binary

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 from gym_unity.envs import UnityEnv
 
-env_name = "./env/Soccer.app"
+env_name = "./env/GridWorld"
 multi_env = UnityEnv(env_name, worker_id=1, 
                      use_visual=False, multiagent=True)
