ea6e4a39770ee06d0abf5524454a5de4ffc0a431
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-10 17:46:24 +0800

    update app

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Get Environemnt Binary
 - MACOS  
-For macos, the build binary file is in `./env/SoccerTwos.app`
+For macos, the build binary file is in `./env/macos/SoccerTwosLearner.app`
 - Ubuntu  
-For Ubuntu, the build binary file is in `./env/SoccerTwos.x86_64`
+For Ubuntu, the build binary file is in `./env/linux/SoccerTwosLearner.x86_64`
 - Windows  
-For Windows, the build binary file is in `./env/SoccerTwos/`
+For Windows, the build binary file is in `./env/SoccerTwosLearner/`
--- env.py
+++ env.py
@@ -5,50 +5,50 @@ from gym_unity.envs import UnityEnv
 from mlagents.envs import UnityEnvironment
 
 train_mode = True
-env_name = "./env/SoccerTwosLearner"
+env_name = './env/macos/SoccerTwosLearner.app'
 
-env = UnityEnvironment(file_name=env_name, )
+env = UnityEnvironment(file_name=env_name)
 
-# Set the default brain to work with
-default_brain = env.brain_names[0]
-print(env.brain_names)
-brain = env.brains[default_brain]
+striker_brain_name, goalie_brain_name = env.brain_names
+striker_brain = env.brains[striker_brain_name]
+goalie_brain = env.brains[goalie_brain_name]
 
+env_info = env.reset(train_mode=train_mode)[striker_brain_name]
 
-
-# Reset the environment
-env_info = env.reset(train_mode=train_mode)[default_brain]
-
-# Examine the state space for the default brain
 print("Agent state looks like: \n{}".format(env_info.vector_observations[0]))
 
-# Examine the observation space for the default brain
 for observation in env_info.visual_observations:
     print("Agent observations look like:")
     if observation.shape[3] == 3:
-        plt.imshow(observation[0,:,:,:])
+        plt.imshow(observation[0, :, :, :])
         plt.show()
     else:
-        plt.imshow(observation[0,:,:,0])
+        plt.imshow(observation[0, :, :, 0])
         plt.show()
 
-
-
 for episode in range(10):
-    env_info = env.reset(train_mode=train_mode)[default_brain]
+    env_info = env.reset(train_mode=train_mode)[striker_brain_name]
     done = False
     episode_rewards = 0
-    while not done:
-        action_size = brain.vector_action_space_size
-        if brain.vector_action_space_type == 'continuous':
-            env_info = env.step(np.random.randn(len(env_info.agents), 
-                                                action_size[0]))[default_brain]
-        else:
-            action = np.column_stack([np.random.randint(0, action_size[i], size=(len(env_info.agents))) for i in range(len(action_size))])
-            env_info = env.step(action)[default_brain]
-        episode_rewards += env_info.rewards[0]
-        done = env_info.local_done[0]
-    print("Total reward this episode: {}".format(episode_rewards))
+    num_agent = len(env_info.agents)
 
+    while not done:
+        action_size = striker_brain.vector_action_space_size
+        action = np.column_stack([
+            np.random.randint(0, action_size[i], size=num_agent)
+            for i in range(len(action_size))
+        ])
+        # env_info = env.step(action)[striker_brain_name]
+        env_info = env.step({
+            striker_brain_name: action,
+            goalie_brain_name: action
+        })
+
+        # episode_rewards += env_info.rewards[0]
+        episode_rewards += env_info[striker_brain_name].rewards[0] + env_info[
+            goalie_brain_name].rewards[0]
+        done = env_info[striker_brain_name].local_done[0]
+        done = env_info[striker_brain_name].local_done[0]
+    print("Total reward this episode: {}".format(episode_rewards))
 
 env.close()
