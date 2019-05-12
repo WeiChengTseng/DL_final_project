6b8c35e404df2287576fc8bdf1ce58f84bc9dfb4
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-12 18:27:23 +0800

    update env

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ import sys
 from gym_unity.envs import UnityEnv
 from mlagents.envs import UnityEnvironment
 
-env_Path = 'env/linux/SoccerTwos.x86_64'
+env_Path = 'env/macos/SoccerTwosLearner.app'
 train_mode = True
 env = UnityEnvironment(file_name=env_Path, worker_id=2)
 
@@ -27,23 +27,23 @@ for observation in env_info.visual_observations:
         plt.show()
 
 for episode in range(10):
-    env_info_str    = env.reset(train_mode=train_mode)[striker_brain_name]
+    env_info_striker    = env.reset(train_mode=train_mode)[striker_brain_name]
     env_info_goalie = env.reset(train_mode=train_mode)[goalie_brain_name]
     
-    done_str = False
+    done_striker = False
     done_goalie = False
     episode_rewards = 0
 
-    num_agent_str = len(env_info_str.agents)
+    num_agent_striker = len(env_info_striker.agents)
     num_agent_goalie = len(env_info_goalie.agents)
 
-    while not (done_str & done_goalie):
-        action_size_str  = striker_brain.vector_action_space_size
+    while not (done_striker & done_goalie):
+        action_size_striker  = striker_brain.vector_action_space_size
         action_size_goalie= goalie_brain.vector_action_space_size
         
-        action_str = np.column_stack([
-            np.random.randint(0, action_size_str[i], size=num_agent_str)
-            for i in range(len(action_size_str))
+        action_striker = np.column_stack([
+            np.random.randint(0, action_size_striker[i], size=num_agent_striker)
+            for i in range(len(action_size_striker))
         ])
         action_goalie = np.column_stack([
             np.random.randint(0, action_size_goalie[i], size=num_agent_goalie)
@@ -51,13 +51,13 @@ for episode in range(10):
         ])
 
         env_info = env.step({
-            striker_brain_name: action_str,
+            striker_brain_name: action_striker,
             goalie_brain_name: action_goalie
         })
 
         episode_rewards += env_info[striker_brain_name].rewards[0] + env_info[
             goalie_brain_name].rewards[0]
-        done_str = env_info[striker_brain_name].local_done[0]
+        done_striker = env_info[striker_brain_name].local_done[0]
         done_goalie = env_info[goalie_brain_name].local_done[0]
     print("Total reward this episode: {}".format(episode_rewards))
 
