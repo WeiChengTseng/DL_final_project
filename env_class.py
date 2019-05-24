6310968b26c392a54261d8c64339efb0b31389b3
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-25 00:42:22 +0800

    format

--- env_class.py
+++ env_class.py
@@ -5,9 +5,13 @@ from gym_unity.envs import UnityEnv
 from mlagents.envs import UnityEnvironment
 
 
-
 class SocTwoEnv():
-    def __init__(self, env_path, worker_id, train_mode=True, n_str=16, n_goalie=16):
+    def __init__(self,
+                 env_path,
+                 worker_id,
+                 train_mode=True,
+                 n_str=16,
+                 n_goalie=16):
         self.env = UnityEnvironment(file_name=env_path, worker_id=0)
         self.striker_brain_name, self.goalie_brain_name = self.env.brain_names
         self.striker_brain = self.env.brains[self.striker_brain_name]
@@ -19,7 +23,7 @@ class SocTwoEnv():
         self.done_hist_goalie = [False] * 16
         self.episode_str_rewards = 0
         self.episode_goalie_rewards = 0
-        
+
         self.n_str = n_str
         self.n_goalie = n_goalie
         self.act_str_hist = [[] for x in range(n_str)]
@@ -48,32 +52,40 @@ class SocTwoEnv():
         return self.env_info
 
     def reward(self):
-        self.episode_str_rewards = np.array(self.env_info[self.striker_brain_name].rewards)
-        self.episode_goalie_rewards = np.array(self.env_info[self.goalie_brain_name].rewards)
+        self.episode_str_rewards = np.array(
+            self.env_info[self.striker_brain_name].rewards)
+        self.episode_goalie_rewards = np.array(
+            self.env_info[self.goalie_brain_name].rewards)
         return self.episode_str_rewards, self.episode_goalie_rewards
+
     def close(self):
         self.env.close()
 
     def done(self):
-        self.done_str = np.array(self.env_info[self.striker_brain_name].local_done)
-        self.done_goalie = np.array(self.env_info[self.goalie_brain_name].local_done)
+        self.done_str = np.array(
+            self.env_info[self.striker_brain_name].local_done)
+        self.done_goalie = np.array(
+            self.env_info[self.goalie_brain_name].local_done)
 
     def reset_some_agents(self, str_arg, goalie_arg):
         for i in str_arg:
             self.act_str_hist[i[0]] = []
         for i in goalie_arg:
             self.act_goalie_hist[i[0]] = []
-    def print_r(self,episode):
-        print("Total reward this episode_{}: {}".format(episode,self.episode_rewards))
-        return 
+
+    def print_r(self, episode):
+        print("Total reward this episode_{}: {}".format(
+            episode, self.episode_rewards))
+        return
 
 
 if __name__ == "__main__":
 
     env_Path = r'.\env\windows\SoccerTwosBirdView\Unity Environment.exe'
-    soc_env = SocTwoEnv(env_Path,worker_id=1,train_mode=True)
+    env_Path = './env/macos/SoccerTwosLearnerBirdView.app.app'
+    soc_env = SocTwoEnv(env_Path, worker_id=1, train_mode=True)
     soc_env.reset()
-    
+
     episode = 0
     while episode < 10:
 
@@ -91,21 +103,23 @@ if __name__ == "__main__":
             (soc_env.act_goalie_hist[i]).append(action_goalie[i][0])
         for i in range(soc_env.n_str):
             (soc_env.act_str_hist[i]).append(action_str[i][0])
-        soc_env.step(action_str,action_goalie)
+        soc_env.step(action_str, action_goalie)
         soc_env.done()
         if True in soc_env.done_goalie:
             soc_env.reward()
-            print("episode", episode, "*"*10)
+            print("episode", episode, "*" * 10)
             arg_done_goalie = np.argwhere(soc_env.done_goalie == True)
             for i in arg_done_goalie:
-                print("which goalie %d act"%(i[0]), soc_env.act_goalie_hist[i[0]], "len", len(soc_env.act_goalie_hist[i[0]]))
+                print("which goalie %d act" % (i[0]),
+                      soc_env.act_goalie_hist[i[0]], "len",
+                      len(soc_env.act_goalie_hist[i[0]]))
                 print("reword", soc_env.episode_goalie_rewards[i][0])
             arg_done_str = np.argwhere(soc_env.done_str == True)
             for i in arg_done_str:
-                print("which str %d act"%(i[0]), soc_env.act_str_hist[i[0]], "len", len(soc_env.act_str_hist[i[0]]))
+                print("which str %d act" % (i[0]), soc_env.act_str_hist[i[0]],
+                      "len", len(soc_env.act_str_hist[i[0]]))
                 print("reword", soc_env.episode_str_rewards[i][0])
             soc_env.reset_some_agents(arg_done_str, arg_done_goalie)
-            print("*"*10)
+            print("*" * 10)
             episode += 1
     soc_env.close()
-
