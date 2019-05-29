d6a87c4c46df3bda08e4681eb7a1be22c75e8912
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-29 18:27:29 +0800

    update atari

--- ppo_atari.py
+++ ppo_atari.py
@@ -7,7 +7,7 @@ from env_exp import SocTwoEnv
 import pickle
 
 env = gym.make('Breakout-ram-v0')
-env.render()
+# env.render()
 
 ############## Hyperparameters Striker ##############
 state_dim_striker = 128
@@ -15,10 +15,10 @@ action_dim_striker = 3
 n_latent_var_striker = 64  # number of variables in hidden layer
 #############################################
 
-max_episodes = 5000  # max training episodes
-log_interval = 10  # print avg reward in the interval
-update_episode = 10  # update policy every n timesteps 2000
-lr = 0.001
+max_episodes = 50000  # max training episodes
+log_interval = 500  # print avg reward in the interval
+update_episode = 50  # update policy every n timesteps 2000
+lr = 2e-3
 gamma = 0.99  # discount factor
 K_epochs = 4  # update policy for K epochs
 eps_clip = 0.2  # clip parameter for PPO
@@ -50,19 +50,18 @@ while i_episode < (max_episodes + 1):
         action_striker = ppo_striker.policy_old.act(state_striker,
                                                     memory_striker)
         states, reward, done, _ = env.step(action_striker)
-        env.render()
+        # env.render()
         memory_striker.update_reward(reward, done)
 
         running_reward += reward
         if done:
-            # print(i_episode)
             break
         # if np.argwhere(done):
         #     i_episode += len(np.argwhere(done))
         #     print(i_episode)
         #     break
     i_episode += 1
-    if (i_episode + 1) % update_episode == 0:
+    if (i_episode) % update_episode == 0:
         # print('update')
         ppo_striker.update(memory_striker)
         memory_striker.clear_memory()
@@ -78,4 +77,4 @@ while i_episode < (max_episodes + 1):
             i_episode, avg_length, running_reward))
         running_reward = 0
         avg_length = 0
-pickle.dumps(reward_history, open('reward_atari.pkl', 'wb'))
+        pickle.dump(reward_history, open('reward_atari.pkl', 'wb'))
