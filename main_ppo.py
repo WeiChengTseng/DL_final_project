7786fed4dba499b8cfd8b68d8067884386f80e54
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-06-01 10:13:56 +0800

    review ppo action space

--- main_ppo.py
+++ main_ppo.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import gym
-from ppo.PPO import PPO, Memory
+from ppo.PPO import PPO
 from ppo.utils import ReplayBuffer
 from env_exp import SocTwoEnv
 
@@ -20,27 +20,27 @@ action_dim_goalie = 5
 n_latent_var_goalie = 64  # number of variables in hidden layer
 #############################################
 
-
-max_episodes = 50000    # max training episodes
-max_timesteps = 300     # max timesteps in one episode
-solved_reward = 230     # stop training if avg_reward > solved_reward
-log_interval = 100      # print avg reward in the interval
-update_timestep = 200   # update policy every n timesteps 2000
+max_episodes = 50000  # max training episodes
+max_timesteps = 300  # max timesteps in one episode
+log_interval = 100  # print avg reward in the interval
+update_timestep = 200  # update policy every n timesteps 2000
 lr = 0.001
-gamma = 0.99            # discount factor
-K_epochs = 4            # update policy for K epochs
-eps_clip = 0.2          # clip parameter for PPO
+gamma = 0.99  # discount factor
+K_epochs = 4  # update policy for K epochs
+eps_clip = 0.2  # clip parameter for PPO
 random_seed = None
 
 if random_seed:
     torch.manual_seed(random_seed)
     env.seed(random_seed)
 
-memory_striker = Memory()
+# memory_striker = Memory()
+memory_striker = ReplayBuffer(16, gamma)
 ppo_striker = PPO(state_dim_striker, action_dim_striker, n_latent_var_striker,
                   lr, gamma, K_epochs, eps_clip)
 
-memory_goalie = Memory()
+# memory_goalie = Memory()
+memory_goalie = ReplayBuffer(16, gamma)
 ppo_goalie = PPO(state_dim_goalie, action_dim_goalie, n_latent_var_goalie, lr,
                  gamma, K_epochs, eps_clip)
 
@@ -56,7 +56,8 @@ for i_episode in range(1, max_episodes + 1):
         timestep += 1
 
         # Running policy_old:
-        action_striker = ppo_striker.policy_old.act(state_striker, memory_striker)
+        action_striker = ppo_striker.policy_old.act(state_striker,
+                                                    memory_striker)
         action_goalie = ppo_goalie.policy_old.act(state_goalie, memory_goalie)
         states, reward, done, _ = env.step(action_striker, action_goalie)
         print(np.argwhere(done))
@@ -76,15 +77,7 @@ for i_episode in range(1, max_episodes + 1):
 
         running_reward += max(reward[0])
 
-
     avg_length += t
-
-    # stop training if avg_reward > solved_reward
-    if running_reward > (log_interval * solved_reward):
-        print("########## Solved! ##########")
-        torch.save(ppo_striker.policy.state_dict(),
-                   './PPO_{}.pth'.format('SoccerTwos'))
-        break
 
     # logging
     if i_episode % log_interval == 0:
@@ -95,3 +88,5 @@ for i_episode in range(1, max_episodes + 1):
             i_episode, avg_length, running_reward))
         running_reward = 0
         avg_length = 0
+        torch.save(ppo_striker.policy.state_dict(),
+                   './PPO_{}.pth'.format('SoccerTwos'))
--- ppo_atari.py
+++ ppo_atari.py
@@ -13,31 +13,26 @@ except:
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 env = gym.make('Breakout-ram-v0')
-# env.render()
+env.render()
 
 ############## Hyperparameters Striker ##############
-state_dim_striker = 128
-action_dim_striker = 3
-n_latent_var_striker = 64  # number of variables in hidden layer
+state_dim = 128
+action_dim = 4
+n_latent_var = 64  # number of variables in hidden layer
 #############################################
 
-max_episodes = 50000  # max training episodes
-log_interval = 500  # print avg reward in the interval
-update_episode = 50  # update policy every n timesteps 2000
+max_episodes = 50000    # max training episodes
+log_interval = 50      # print avg reward in the interval
+update_episode = 20     # update policy every n timesteps 2000
 lr = 2e-3
-gamma = 0.99  # discount factor
-K_epochs = 4  # update policy for K epochs
+gamma = 0.99    # discount factor
+K_epochs = 4    # update policy for K epochs
 eps_clip = 0.2  # clip parameter for PPO
-random_seed = None
-
-if random_seed:
-    torch.manual_seed(random_seed)
-    env.seed(random_seed)
 
 # memory_striker = Memory()
 memory_striker = ReplayBuffer(1, gamma)
-ppo_striker = PPO(state_dim_striker, action_dim_striker, n_latent_var_striker,
-                  lr, gamma, K_epochs, eps_clip, device)
+ppo_agent = PPO(state_dim, action_dim, n_latent_var, lr, gamma, K_epochs,
+                eps_clip, device)
 
 # logging variables
 running_reward = 0
@@ -49,31 +44,26 @@ reward_history = []
 i_episode = 1
 while i_episode < (max_episodes + 1):
     state_striker = env.reset()
-    # for t in range(max_timesteps):
     while True:
         timestep += 1
-
-        action_striker = ppo_striker.policy_old.act(state_striker,
-                                                    memory_striker)
+        action_striker = ppo_agent.policy_old.act(state_striker,
+                                                  memory_striker)
         states, reward, done, _ = env.step(action_striker)
-        # env.render()
+        env.render()
         memory_striker.update_reward(reward, done)
 
         running_reward += reward
         if done:
             break
-        # if np.argwhere(done):
-        #     i_episode += len(np.argwhere(done))
-        #     print(i_episode)
-        #     break
+
     i_episode += 1
     if (i_episode) % update_episode == 0:
-        # print('update')
-        ppo_striker.update(memory_striker)
+        ppo_agent.update(memory_striker)
         memory_striker.clear_memory()
 
     avg_length += timestep
     timestep = 0
+
     # logging
     if i_episode % log_interval == 0:
         avg_length = avg_length / log_interval
