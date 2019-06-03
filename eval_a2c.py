18617dab3261bf816f638997bd04745796413990
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-06-03 10:31:42 +0800

    eval compete agents

--- eval_a2c.py
+++ eval_a2c.py
@@ -15,6 +15,7 @@ from a2c.train_multi import train
 
 from env_exp import SocTwoEnv
 
+
 def eval_with_random_agent(net_striker,
                            net_goalie,
                            env,
@@ -97,24 +98,87 @@ def eval_self_complete(net_striker,
     return
 
 
+def eval_agents_compete(strikers,
+                        goalies,
+                        env,
+                        device,
+                        order='team',
+                        eval_epsoid=40):
+    obs_striker, obs_goalie = env.reset(order)
+    policies_striker = [None, None]
+    policies_goalie = [None, None]
+
+    epsoid = 0
+    while epsoid < eval_epsoid:
+        obs_striker = Variable(
+            torch.from_numpy(obs_striker).float()).to(device)
+        obs_goalie = Variable(torch.from_numpy(obs_goalie).float()).to(device)
+
+        policies_striker[0], _ = strikers[0](obs_striker[:8])
+        policies_goalie[0], _ = goalies[0](obs_goalie[:8])
+        policies_striker[1], _ = strikers[1](obs_striker[8:])
+        policies_goalie[1], _ = goalies[1](obs_goalie[8:])
+
+        policy_strikers = torch.cat(policies_striker, dim=0)
+        policy_goalies = torch.cat(policies_goalie, dim=0)
+
+        probs_striker = F.softmax(policy_strikers, dim=-1)
+        probs_goalie = F.softmax(policy_goalies, dim=-1)
+
+        actions_striker = probs_striker.multinomial(1).data
+        actions_goalie = probs_goalie.multinomial(1).data
+
+        obs, rewards, dones, _ = env.step(actions_striker, actions_goalie,
+                                          order)
+        obs_striker, obs_goalie = obs
+
+        rewards_striker = torch.from_numpy(
+            rewards[0]).float().unsqueeze(1).to(device)
+        rewards_goalie = torch.from_numpy(
+            rewards[1]).float().unsqueeze(1).to(device)
+
+        for i in np.argwhere(dones[0]).flatten():
+            epsoid += 1
+    return
+
+
 if __name__ == '__main__':
     env_path = './env/macos/SoccerTwosFast.app'
     env = SocTwoEnv(env_path, worker_id=0, train_mode=False, render=True)
     # net_path = './a2c/ckpt/a2c_step20320000.pth'
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net_path = './a2c/ckpt_reward_shaping/a2c_step39960000.pth'
-    # net_path = './a2c/ckpt_rs_large/a2cLarge_step36960000.pth'
+    net_path_large = './a2c/ckpt_rs_large/a2cLarge_step36960000.pth'
 
-    policy_striker, policy_goalie = A2C(7).to(device), A2C(5).to(device)
-    # policy_striker, policy_goalie = A2CLarge(7).to(device), A2CLarge(5).to(
-    #     device)
+    with torch.no_grad():
+        policy_striker, policy_goalie = A2C(7).to(device), A2C(5).to(device)
+        policy_striker_large, policy_goalie_large, = A2CLarge(7).to(
+            device), A2CLarge(5).to(device)
 
-    checkpoint = torch.load(net_path, map_location=device)
-    policy_striker.load_state_dict(checkpoint['striker_a2c'])
-    policy_goalie.load_state_dict(checkpoint['goalie_a2c'])
+        ckpt_large = torch.load(net_path_large, map_location=device)
+        policy_striker_large.load_state_dict(ckpt_large['striker_a2c'])
+        policy_goalie_large.load_state_dict(ckpt_large['goalie_a2c'])
 
-    policy_striker.eval()
-    policy_goalie.eval()
-    eval_with_random_agent(policy_striker, policy_goalie, env, device)
-    # eval_self_complete(policy_striker, policy_goalie, env, device, 'team')
+        ckpt = torch.load(net_path, map_location=device)
+        policy_striker.load_state_dict(ckpt['striker_a2c'])
+        policy_goalie.load_state_dict(ckpt['goalie_a2c'])
+
+        policy_striker_large.eval()
+        policy_goalie_large.eval()
+
+        policy_striker.eval()
+        policy_goalie.eval()
+
+        # eval_with_random_agent(policy_striker,
+        #                        policy_goalie,
+        #                        env,
+        #                        device,
+        #                        eval_epsoid=100)
+        # eval_self_complete(policy_striker, policy_goalie, env, device, 'team')
+        eval_agents_compete([policy_striker_large, policy_striker],
+                            [policy_goalie_large, policy_goalie],
+                            env,
+                            device,
+                            order='team',
+                            eval_epsoid=40)
     pass
