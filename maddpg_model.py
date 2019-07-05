b1abfdc7873103230454e04f55e80527806d8406
pohan <aapp1420@gmail.com>
2019-07-05 20:18:19 +0800

    policy

--- maddpg_model.py
+++ maddpg_model.py
@@ -4,16 +4,17 @@ import torch.nn.functional as F
 
 
 class Critic(nn.Module):
-    def __init__(self, n_agent, dim_observation = 112, dim_action = 7):
+    def __init__(self, ns_agent,ng_agent, dim_observation = 112, s_dim_action = 7,g_dim_action=5):
         super(Critic, self).__init__()
-        self.n_agent = n_agent
+        self.n_agent = ns_agent+ng_agent
         self.dim_observation = dim_observation
-        self.dim_action = dim_action
-        obs_dim = dim_observation * n_agent
-        act_dim = self.dim_action * n_agent
-        self.FC = nn.Linear(obs_dim, 128)
+        obs_dim = dim_observation * self.n_agent
+        act_dim = s_dim_action * ns_agent + g_dim_action *ng_agent
+        self.FC = nn.Linear(obs_dim, 256)
         self.sq = nn.Sequential(
-            nn.Linear(128+act_dim , 64),
+            nn.Linear(256+act_dim , 128),
+            nn.ReLU(),
+            nn.Linear(128,64),
             nn.ReLU(),
             nn.Linear(64,1),
         )
@@ -32,18 +33,15 @@ class Goalie(nn.Module):
             nn.Linear(128,64),
             nn.ReLU(),
             nn.Linear(64,dim_action),
-            nn.LogSoftmax(-1),
+            nn.LogSoftmax(-1)
         )
 
     # action output between -2 and 2
     def forward(self, obs):
-        output = torch.zeros((2,))
         buf = self.sq(obs)
         buf = buf.squeeze()
-        new=torch.zeros((buf.size(0),2))
-        new.fill_(1e-12)
-        output = torch.cat((buf,new),dim=-1)
-        return output
+        
+        return F.gumbel_softmax(buf,tau=5,dim=-1)
 
 
 
@@ -61,5 +59,5 @@ class Striker(nn.Module):
 
     # action output between -2 and 2
     def forward(self, obs):
-        return self.sq(obs)
+        return F.gumbel_softmax(self.sq(obs),tau=5,dim=-1)
 
